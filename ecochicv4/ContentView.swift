022b0a96f2160758ffee0aefaaf20d068c6e1787
Manuel <manuelmacedoteran@gmail.com>
2024-12-27 14:29:23 -0700

    Initial Commit

---
+++ ecochicv4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ecochicv4
+//
+//  Created by Manuel Teran on 2024-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ecochicv4/ecochicv4App.swift
@@ -0,0 +1,17 @@
+//
+//  ecochicv4App.swift
+//  ecochicv4
+//
+//  Created by Manuel Teran on 2024-12-27.
+//
+
+import SwiftUI
+
+@main
+struct ecochicv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
